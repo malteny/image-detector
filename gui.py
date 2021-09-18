170f094f5f7f554ffaf9670bb00ef0a6a48f50c4
MALTE <maltenygren01@gmail.com>
2021-09-18 19:32:39 +0200

    without styling

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+# 21-09-18: Problems with styling, tkk.Style() does not seem to apply.
 
 import tkinter as tk
 from tkinter import ttk
@@ -5,13 +6,16 @@ from tkinter import ttk
 
 def create_text_frame(container):
     '''Create frame with instruction text'''
-    frame = ttk.Frame(container)
+
+    frame = ttk.Frame(
+        container,
+        style = 'TFrame'
+    )
 
     label = ttk.Label(
         frame,
         text = 'Browse for a photo to identify',
-        background = container['bg'],
-        font = ('Arial', 20)
+        style = 'TLabel'
     )
 
     label.pack()
@@ -21,7 +25,11 @@ def create_text_frame(container):
 
 def create_button_frame(container):
     '''Create frame with "browse" button'''
-    frame = ttk.Frame(container)
+
+    frame = ttk.Frame(
+        container,
+        style = 'TFrame'
+    )
 
     button = ttk.Button(
         frame,
@@ -34,18 +42,31 @@ def create_button_frame(container):
     return frame
 
 
+def create_main_frame(container):
+    ''' Creates main frame inside window '''
+
+    frame = ttk.Frame(
+        container,
+        padding = (5,5,5,5),
+        style = 'TFrame',
+        height = 50,
+        width = 40
+        )
+
+    return frame
+
+
 def create_main_window():
-    '''Create frame with main window'''
+    '''Create window and start loop'''
+
     window = tk.Tk()
     window.title('image-detector')
-    window.geometry('500x200')
-    window.configure(bg='#D8BFD8')
+    window.geometry('400x200')
+    s = ttk.Style()
 
-    text = create_text_frame(window)
-    button = create_button_frame(window)
-
-    text.grid( row = 0, column = 0 )
-    button.grid( row = 1, column = 2 )
+    main_frame = create_main_frame(window).pack()
+    create_text_frame(main_frame).pack()
+    create_button_frame(main_frame).pack()
 
     window.mainloop()
 

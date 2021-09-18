17496f81d8ef3cef5b23c981772a47b0b04715cf
MALTE <maltenygren01@gmail.com>
2021-09-19 00:54:02 +0200

    Functioning analyzer

--- analyzer.py
+++ analyzer.py
@@ -3,7 +3,10 @@ import json
 
 
 def analyze_image(file_path):
-    ''' Sends file specified by path to openvisionapi and returns result '''
+    '''
+    Sends file specified by path to openvisionapi and returns
+    tuple of prediction and accuracy.
+    '''
     URL = 'https://api.openvisionapi.com'
     PATH = '/api/v1/detection'
 
@@ -12,13 +15,14 @@ def analyze_image(file_path):
 
     url = URL + PATH
 
-    files = {'image': (file_path, image_bytes, 'image/jpg')}
+    files = {'image': (file_path, image_bytes, 'image/jpeg')}
 
     body = {'model': 'yolov4'}
 
     response = requests.post(url=url, files=files, data=body)
 
-    print(response)
+    json_response = response.json()
 
-
-analyze_image('cat.jpg')
+    if json_response['description'] == 'Detected objects':
+        return (json_response['predictions']['label'],
+                json_response['predictions']['score'])
